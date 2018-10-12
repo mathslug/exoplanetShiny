02edc571811ead2337ef54f39bf7a88769cbe078
mathSlug <johnsumnerbentley@gmail.com>
2018-10-12 16:09:14 -0400

    added first two graphs and formatting updates

--- server.R
+++ server.R
@@ -1,9 +1,46 @@
 function(input, output) {
-  set.seed(122)
-  histdata <- rnorm(500)
   
   output$bar1 = renderPlot({
+    #filter by selected year
     bar1data = scatter_data %>% filter(., discovery_year == input$slider1)
-    ggplot(bar1data, aes(x = detection_type)) + geom_bar()
+
+    #produce plot
+    ggplot(bar1data, aes(x = detection_type)) + geom_bar() + theme(plot.subtitle = element_text(vjust = 1), 
+    plot.caption = element_text(vjust = 1), 
+    panel.grid.major = element_line(colour = "white", 
+        linetype = "blank"), panel.grid.minor = element_line(linetype = "blank"), 
+    axis.title = element_text( 
+        size = 9, colour = "white"), axis.text.x = element_text( 
+        size = 8, colour = "white", vjust = 0.75, 
+        angle = 45), axis.text.y = element_text( 
+        colour = "white"), plot.title = element_text( 
+        size = 13, colour = "white", hjust = .5), panel.background = element_rect(fill = "black"), 
+    plot.background = element_rect(fill = "black")) +labs(title = paste("Number of Planets Discovered in", input$slider1), 
+    x = "Detection Method", y = "Planet Count", 
+    colour = "White")
   })
+  
+  
+  output$scatter1 = renderPlot({
+    #organize data, only include planets with known mass
+    scatter1data = scatter_data %>% filter(., discovery_year <= input$slider1,
+                                           !is.na(planet_mass))
+    
+    #create plot
+    ggplot(scatter1data, aes(x = discovery_year, y = planet_mass, color = detection_type)) +
+      geom_point(position = 'jitter') + theme(plot.subtitle = element_text(vjust = 1), 
+    plot.caption = element_text(vjust = 1), 
+    plot.title = element_text(hjust = 0.5), 
+    legend.title = element_text(size = 8), 
+    plot.background = element_rect(fill = "ghostwhite"), 
+    legend.key = element_rect(fill = "ghostwhite"), 
+    legend.background = element_rect(fill = "ghostwhite"), 
+    legend.position = "bottom", legend.direction = "horizontal") +labs(title = paste("Discoveries Through", input$slider1, "Year by Method"), 
+    x = "Year of Discovery", y = "Planet Mass (M_Jup)", 
+    colour = NULL) + theme(panel.grid.major = element_line(linetype = "blank"), 
+    panel.grid.minor = element_line(linetype = "blank"), 
+    panel.background = element_rect(fill = "ghostwhite")) +
+      theme(panel.background = element_rect(fill = "white")) +
+      theme(legend.text = element_text(size = 9), 
+    legend.background = element_rect(size = 0.7))  })
 }
--- ui.R
+++ ui.R
@@ -3,19 +3,24 @@ library(googleVis)
 
 
 dashboardPage(
-  dashboardHeader(title = "Basic dashboard"),
-  dashboardSidebar(),
+  dashboardHeader(title = "Exoplanet Search"),
+  dashboardSidebar(collapsed = TRUE),
   dashboardBody(
     # Boxes need to be put in a row (or column)
     fluidRow(
       box(
         width = 12,
-        title = "Year of Data",
+        title = "Visualize the New and Total Exoplanet Discoveries by Year
+          and Detection Method",
+        solidHeader = TRUE,
+        collapsible = TRUE,
+        collapsed = TRUE,
         sliderInput("slider1", "Select Year", min(use_data$discovery_year),
-                    max(use_data$discovery_year), 2000, ticks = FALSE,
+                    max(use_data$discovery_year), 2018, ticks = FALSE,
                     round = TRUE, sep = '', width = '100%'))),
     fluidRow(  
-      box(plotOutput("bar1", height = 250))
+      box(plotOutput("bar1", height = 400), background = "black"),
+      box(plotOutput("scatter1", height = 400))
     )
   )
 )
