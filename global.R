41886b11c2aa4cd3369487f78d8cf1351c372168
mathSlug <johnsumnerbentley@gmail.com>
2018-10-12 12:12:26 -0400

    started ignoring data

---
+++ global.R
@@ -0,0 +1,7 @@
+library(dplyr)
+library(ggplot2)
+library(shiny)
+library(shinydashboard)
+library(googleVis)
+
+raw_data = read.csv("https://github.com/mathSlug/exoplanetShiny/blob/master/kepler.csv")
