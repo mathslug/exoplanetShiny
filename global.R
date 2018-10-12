d7d1b83994f72a21c4650a3e62f29ca6ec4546aa
mathSlug <johnsumnerbentley@gmail.com>
2018-10-12 18:40:19 -0400

    added custom.css

--- global.R
+++ global.R
@@ -8,7 +8,8 @@ raw_data = read.csv("data/kepler.csv")
 use_data = raw_data %>% filter(., planet_status == "Confirmed") %>%
   select(., star_name, star_distance, planet_name = X..name, detection_type,
          planet_mass = mass,planet_radius = radius,
-         discovery_year = discovered, temp_calculated, temp_measured)
+         discovery_year = discovered, temp_calculated, temp_measured) %>%
+  mutate(., log_planet_mass = log(planet_mass))
 
 #add one missing discovery year. Earliest publication I could find regarding
 #this object is in 2018.
@@ -16,4 +17,4 @@ use_data[use_data$planet_name == "OGLE-2017-BLG-1434L b", "discovery_year"] = 20
 
 #data for scatterplots
 scatter_data = use_data %>% select(., discovery_year, detection_type,
-                                   planet_mass)
+                                   planet_mass, log_planet_mass)
--- ui.R
+++ ui.R
@@ -10,29 +10,46 @@ dashboardPage(
     
     sidebarUserPanel("by John", image = "earth.jpg"),
     
+    #initialize tabs on sidebar
     sidebarMenu(
-      menuItem("Map", tabName = "map", icon = icon("map")),
+      menuItem("Discoveries", tabName = "discovs", icon = icon("th-large", lib = "glyphicon")),
       menuItem("Data", tabName = "data", icon = icon("database")))
     ),
   
   dashboardBody(
-    #Create first row, the year-slider
-    fluidRow(
-      box(
-        width = 12,
-        title = "Visualize the New and Total Exoplanet Discoveries by Year
-          and Detection Method",
-        solidHeader = TRUE,
-        collapsible = TRUE,
-        collapsed = TRUE,
-        sliderInput("slider1", "Select Year", min(use_data$discovery_year),
-                    max(use_data$discovery_year), 2018, ticks = FALSE,
-                    round = TRUE, sep = '', width = '100%'))),
+    tags$head(
+      tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
+    ),
     
-    #second row, two graphs
-    fluidRow(  
-      box(plotOutput("bar1", height = 400), background = "black"),
-      box(plotOutput("scatter1", height = 400))
+    #Make multiple tab output pages
+    tabItems(
+      
+      #Make first page, with graphs by discovery year
+      tabItem(
+        tabName = "discovs",
+        
+        #Create first row, the year-slider
+        fluidRow(
+          box(
+            width = 12,
+            title = "Visualize the New and Total Exoplanet Discoveries by Year
+            and Detection Method",
+            solidHeader = TRUE,
+            collapsible = TRUE,
+            collapsed = TRUE,
+            sliderInput("slider1", "Select Year", min(use_data$discovery_year),
+                        max(use_data$discovery_year), 2018, ticks = FALSE,
+                        round = TRUE, sep = '', width = '100%'))),
+        
+        #second row, two graphs
+        fluidRow(  
+          box(plotOutput("bar1", height = 400), background = "black"),
+          box(plotOutput("scatter1", height = 400))
+        )
+      )
+      
+      
+      
     )
   )
 )
